50c40325748f5601ad991e2b34532965272eb651
stlgolfer <stlgolfer@live.com>
2017-04-06 21:44:11 -0500

    Got single-line working again. Getting outofbounds now

--- SoupLanguage/src/xyz/amtstl/soup/Soup.java
+++ SoupLanguage/src/xyz/amtstl/soup/Soup.java
@@ -8,16 +8,17 @@ import java.util.List;
 
 import xyz.amtstl.soup.exceptions.SoupSyntaxException;
 import xyz.amtstl.soup.exceptions.SoupVariableException;
-import xyz.amtstl.soup.logic.LanguageController;
+import xyz.amtstl.soup.logic.LanguageDictionary;
 import xyz.amtstl.soup.logic.LogicController;
 import xyz.amtstl.soup.misc.IO;
 
 public class Soup {
-	/*private static String[] numbers;
-	private static String single;*/
 	private static int lineNumber = 1;
+	private static int indexCache = 0;
+	
+	// controllers
 	static LogicController logic = new LogicController();
-	//static int indexCache = 0;
+	static LanguageDictionary lang = new LanguageDictionary();
 	
 	@SuppressWarnings("static-access")
 	public static void main(String args[]) throws Exception {
@@ -26,58 +27,72 @@ public class Soup {
 		//FileReader reader = new FileReader("C:/Users/amigala/Desktop/Github/Soup/Files/program.soup");
 		BufferedReader buff = new BufferedReader(reader);
 		
-		while (true) {		
+		while (true) {
 			final String cache = buff.readLine();
 			
 			try {
 				for (int i = 0; i < cache.length(); i++) {
+					IO.println("Current Pos: " + String.valueOf(i) + " Cache Length: " + String.valueOf(cache.length()));
 					char c = cache.charAt(i);
-					parseFunc(c, i, cache);
-					i = logic.getIndex();
-					
+					checkToken(i, cache, c);
+					i = indexCache;
 				}
 			} catch (NullPointerException ex) {
 				System.exit(0);
 			}
-		
+			
+			indexCache = 0;
 			lineNumber++;
 			logic.setIndex(0);
 		}
 	}
 	
+	@SuppressWarnings("static-access")
 	public static void parseFunc(char c, int i, String cache) throws NumberFormatException, SoupVariableException, SoupSyntaxException {
 		switch (c) {
 			case '!' : // add two numbers
 				logic.soupAdd(i, cache);
 				break;
-		case '@' : // subtract two numbers
-			logic.soupSubtract(i, cache);
-			break;
-		case '#' : // multiply two numbers
-			logic.soupMultiply(i, cache);			
-			break;
-		case '$' : // divide two numbers
-			logic.soupDivide(i, cache);			
-			break;
-		case '%' : // pow one number
-			logic.soupPow(i, cache);			
-			break;
-		case '^' : // basic logarithm
-			logic.soupLog(i, cache);			
-			break;
-		case '[' : // basic if statement
-			logic.soupIf(i, cache);
-			break;
-		case '&' :
-			logic.soupPrint(i, cache);
-			break;
-		case 'v': // gets a variable
-			logic.soupRetrieveVar(i, cache);
-			break;
-		case '.' :
-			break;
-		default :
-			throw new SoupSyntaxException(cache.charAt(i), i, lineNumber);
+			case '@' : // subtract two numbers
+				logic.soupSubtract(i, cache);
+				break;
+			case '#' : // multiply two numbers
+				logic.soupMultiply(i, cache);			
+				break;
+			case '$' : // divide two numbers
+				logic.soupDivide(i, cache);			
+				break;
+			case '%' : // pow one number
+				logic.soupPow(i, cache);			
+				break;
+			case '^' : // basic logarithm
+				logic.soupLog(i, cache);			
+				break;
+			case '[' : // basic if statement
+				logic.soupIf(i, cache);
+				break;
+			case '&' :
+				logic.soupPrint(i, cache);
+				break;
+			case 'v': // gets a variable
+				logic.soupRetrieveVar(i, cache);
+				break;
+			case '.' :
+				break;
+			default :
+				throw new SoupSyntaxException(cache.charAt(i), i, lineNumber);
+		}
+	}
+	
+	public static void checkToken(int i, String cache, char c) throws NumberFormatException, SoupVariableException, SoupSyntaxException {
+		for (int e = 0; i < lang.languageTokens.size(); i++) {
+			if (cache.charAt(i) == lang.languageTokens.get(e)) {
+				parseFunc(c, i, cache);
+				indexCache = logic.getIndex();
+			}
+			else {
+				indexCache += 1;
+			}
 		}
 	}
 }
---
+++ SoupLanguage/src/xyz/amtstl/soup/logic/LanguageDictionary.java
@@ -0,0 +1,27 @@
+package xyz.amtstl.soup.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageDictionary {
+	public static List<Character> languageTokens;
+	
+	public LanguageDictionary() {
+		languageTokens = new ArrayList<Character>();
+		
+		addToken('!');
+		addToken('@');
+		addToken('#');
+		addToken('$');
+		addToken('%');
+		addToken('^');
+		addToken('&');
+		addToken('[');
+		addToken('.');
+		addToken('v');
+	}
+	
+	public static void addToken(char token) {
+		languageTokens.add(token);
+	}
+}
